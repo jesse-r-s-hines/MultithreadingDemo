07aafff36d2eb4ca8434c24d7da24b7b8b04b569
Jesse Hines <jesse.r.s.hines@gmail.com>
2022-04-14 20:45:33 -0400

    Add redirect for 404

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -91,6 +91,17 @@ namespace Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/Home";
+                    await next();
+                }
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
